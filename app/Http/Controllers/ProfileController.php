3786a9ede2fba0ad39c6cc1e9aff66cf1cc986d7
Sachin Jadhav3 <Sachin_Jadhav3@symantec.com>
2018-07-12 08:18:43 +0530

    date diff format changed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -59,7 +59,7 @@ class ProfileController extends Controller
 				->join('posts', 'posts.fbid', '=', 'profiles.fbid')
 				->get();
 				$interval = strtotime($userProfileData[0]->expires_at) - $current_date;
-				$userProfileData[0]->expires_at =  $interval <= 0 ? "Expired" : $userProfileData[0]->expires_at;
+				$userProfileData[0]->expires_at =  $interval <= 0 ? "Expired" : $this->dateDiff(strtotime($userProfileData[0]->expires_at) , $current_date, 2);
 
 			}
 			else{
@@ -73,6 +73,71 @@ class ProfileController extends Controller
 		}
 	}
 	
+	public function dateDiff($time1, $time2, $precision = 6) {
+		// If not numeric then convert texts to unix timestamps
+		if (!is_int($time1)) {
+		  $time1 = strtotime($time1);
+		}
+		if (!is_int($time2)) {
+		  $time2 = strtotime($time2);
+		}
+	
+		// If time1 is bigger than time2
+		// Then swap time1 and time2
+		if ($time1 > $time2) {
+		  $ttime = $time1;
+		  $time1 = $time2;
+		  $time2 = $ttime;
+		}
+	
+		// Set up intervals and diffs arrays
+		$intervals = array('year','month','day','hour','minute','second');
+		$diffs = array();
+	
+		// Loop thru all intervals
+		foreach ($intervals as $interval) {
+		  // Create temp time from time1 and interval
+		  $ttime = strtotime('+1 ' . $interval, $time1);
+		  // Set initial values
+		  $add = 1;
+		  $looped = 0;
+		  // Loop until temp time is smaller than time2
+		  while ($time2 >= $ttime) {
+			// Create new temp time from time1 and interval
+			$add++;
+			$ttime = strtotime("+" . $add . " " . $interval, $time1);
+			$looped++;
+		  }
+	 
+		  $time1 = strtotime("+" . $looped . " " . $interval, $time1);
+		  $diffs[$interval] = $looped;
+		}
+		
+		$count = 0;
+		$times = array();
+		// Loop thru all diffs
+		foreach ($diffs as $interval => $value) {
+		  // Break if we have needed precission
+		  if ($count >= $precision) {
+			break;
+		  }
+		  // Add value and interval 
+		  // if value is bigger than 0
+		  if ($value > 0) {
+			// Add s if value is not 1
+			if ($value != 1) {
+			  $interval .= "s";
+			}
+			// Add value and interval to times array
+			$times[] = $value . " " . $interval;
+			$count++;
+		  }
+		}
+	
+		// Return string with times
+		return implode(", ", $times);
+	  }
+
 	
 	public function getCheckIfConnected(Request $request){
 		try {
--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -39,6 +39,71 @@ class StatusController extends Controller
 		}
 	}
 
+	public function dateDiff($time1, $time2, $precision = 6) {
+		// If not numeric then convert texts to unix timestamps
+		if (!is_int($time1)) {
+		  $time1 = strtotime($time1);
+		}
+		if (!is_int($time2)) {
+		  $time2 = strtotime($time2);
+		}
+	
+		// If time1 is bigger than time2
+		// Then swap time1 and time2
+		if ($time1 > $time2) {
+		  $ttime = $time1;
+		  $time1 = $time2;
+		  $time2 = $ttime;
+		}
+	
+		// Set up intervals and diffs arrays
+		$intervals = array('year','month','day','hour','minute','second');
+		$diffs = array();
+	
+		// Loop thru all intervals
+		foreach ($intervals as $interval) {
+		  // Create temp time from time1 and interval
+		  $ttime = strtotime('+1 ' . $interval, $time1);
+		  // Set initial values
+		  $add = 1;
+		  $looped = 0;
+		  // Loop until temp time is smaller than time2
+		  while ($time2 >= $ttime) {
+			// Create new temp time from time1 and interval
+			$add++;
+			$ttime = strtotime("+" . $add . " " . $interval, $time1);
+			$looped++;
+		  }
+	 
+		  $time1 = strtotime("+" . $looped . " " . $interval, $time1);
+		  $diffs[$interval] = $looped;
+		}
+		
+		$count = 0;
+		$times = array();
+		// Loop thru all diffs
+		foreach ($diffs as $interval => $value) {
+		  // Break if we have needed precission
+		  if ($count >= $precision) {
+			break;
+		  }
+		  // Add value and interval 
+		  // if value is bigger than 0
+		  if ($value > 0) {
+			// Add s if value is not 1
+			if ($value != 1) {
+			  $interval .= "s";
+			}
+			// Add value and interval to times array
+			$times[] = $value . " " . $interval;
+			$count++;
+		  }
+		}
+	
+		// Return string with times
+		return implode(", ", $times);
+	  }
+
 	public function getAllFriendsStatus(Request $request){
 		try{
 
@@ -61,6 +126,7 @@ class StatusController extends Controller
 			foreach ($allPosts as $post) {
 				$interval = strtotime($post->expires_at) - $current_date;
 				if ( strpos($connections,  '->'.$post->fbid.'->') !== false &&  $interval >= 0 ) {
+					$post->expires_at = $this->dateDiff(strtotime($post->expires_at) , $current_date, 2);
 					array_push($usersStatus, $post);
 				}
 			  }
