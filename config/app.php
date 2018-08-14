a133f2a0c54456067125ba487617223d141ff6e9
Sachin Jadhav3 <Sachin_Jadhav3@symantec.com>
2018-08-14 23:02:55 +0530

    fcm notification package config addecd

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-       //LaravelFCM\FCMServiceProvider::class,
+       LaravelFCM\FCMServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        //'FCM'      => LaravelFCM\Facades\FCM::class,
+        'FCM'      => LaravelFCM\Facades\FCM::class,
     	//'FCMGroup' => LaravelFCM\Facades\FCMGroup::class,
     ],
 
