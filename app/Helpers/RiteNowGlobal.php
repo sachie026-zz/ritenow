c7f86782623469a19fa0e50c637bdffe638cf9a1
Sachin Jadhav3 <Sachin_Jadhav3@symantec.com>
2018-08-15 12:32:25 +0530

    added try catch for notificaiton function

--- app/Helpers/RiteNowGlobal.php
+++ app/Helpers/RiteNowGlobal.php
@@ -65,36 +65,42 @@ class RiteNowGlobal {
 
 	public static function sendNotificationToDevice( $tokens, $msg){
 
-		$optionBuilder = new OptionsBuilder();
-		$optionBuilder->setTimeToLive(60*20);
+		try{
+			$optionBuilder = new OptionsBuilder();
+			$optionBuilder->setTimeToLive(60*20);
+	
+			$notificationBuilder = new PayloadNotificationBuilder('Ritenow');
+			$notificationBuilder->setBody($msg)
+								->setSound('default');
+	
+			$dataBuilder = new PayloadDataBuilder();
+			$dataBuilder->addData(['a_data' => ""]);
+	
+			$dataBuilder->addData(['content-available' => "1"]);
+	
+			$option = $optionBuilder->build();
+			$notification = $notificationBuilder->build();
+			$data = $dataBuilder->build();
+	
+			$downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
+	
+			$downstreamResponse->numberSuccess();
+			$downstreamResponse->numberFailure();
+			$downstreamResponse->numberModification();
+	
+			//return Array - you must remove all this tokens in your database
+			$downstreamResponse->tokensToDelete();
+	
+			//return Array (key : oldToken, value : new token - you must change the token in your database )
+			$downstreamResponse->tokensToModify();
+	
+			//return Array - you should try to resend the message to the tokens in the array
+			$downstreamResponse->tokensToRetry();
+	
+		}
+		catch(Exception $e){
 
-		$notificationBuilder = new PayloadNotificationBuilder('Ritenow');
-		$notificationBuilder->setBody($msg)
-							->setSound('default');
-
-		$dataBuilder = new PayloadDataBuilder();
-		$dataBuilder->addData(['a_data' => ""]);
-
-		$dataBuilder->addData(['content-available' => "1"]);
-
-		$option = $optionBuilder->build();
-		$notification = $notificationBuilder->build();
-		$data = $dataBuilder->build();
-
-		$downstreamResponse = FCM::sendTo($tokens, $option, $notification, $data);
-
-		$downstreamResponse->numberSuccess();
-		$downstreamResponse->numberFailure();
-		$downstreamResponse->numberModification();
-
-		//return Array - you must remove all this tokens in your database
-		$downstreamResponse->tokensToDelete();
-
-		//return Array (key : oldToken, value : new token - you must change the token in your database )
-		$downstreamResponse->tokensToModify();
-
-		//return Array - you should try to resend the message to the tokens in the array
-		$downstreamResponse->tokensToRetry();
+		}
 
 		// return Array (key:token, value:errror) - in production you should remove from your database the tokens
 	}
